7f05ee022f3cfc34e28e5d89b77de1502ca1fec2
LuisGuilh3rme <luislebert01@outlook.com>
2024-04-21 17:46:56 -0300

    chore: add build and environment settings

---
+++ app.config.ts
@@ -0,0 +1,19 @@
+import { ExpoConfig, ConfigContext } from 'expo/config';
+
+const firebaseApiId = process.env.FIREBASE_API_ID ?? process.env.EXPO_PUBLIC_FIREBASE_API_ID;
+const firebaseApiKey = process.env.FIREBASE_API_KEY ?? process.env.EXPO_PUBLIC_FIREBASE_API_KEY;
+const firebaseMessagingId = process.env.FIREBASE_MESSAGING_ID ?? process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_ID;
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "school-learn-plataform",
+  slug: "school-learn-plataform",
+  extra: {
+    firebaseApiId,
+    firebaseApiKey,
+    firebaseMessagingId,
+    eas: {
+      projectId: "21fda8ed-7351-4728-bf98-25f50940e5eb"
+    }
+  }
+});

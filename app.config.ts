d0a23f8b35bbc0eed965d1908cc197b77c7242c4
LuisGuih3rme <luislebert01@outlook.com>
2024-04-27 23:55:44 -0300

    refactor: change firebase variable "firebaseApiId" to "firebaseAppId"

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 import { ExpoConfig, ConfigContext } from "expo/config";
 
-const firebaseApiId =
-  process.env.FIREBASE_API_ID ?? process.env.EXPO_PUBLIC_FIREBASE_API_ID;
+const firebaseAppId =
+  process.env.FIREBASE_API_ID ?? process.env.EXPO_PUBLIC_FIREBASE_APP_ID;
 const firebaseApiKey =
   process.env.FIREBASE_API_KEY ?? process.env.EXPO_PUBLIC_FIREBASE_API_KEY;
 const firebaseMessagingId =
@@ -13,7 +13,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: "school-learn-plataform",
   slug: "school-learn-plataform",
   extra: {
-    firebaseApiId,
+    firebaseAppId,
     firebaseApiKey,
     firebaseMessagingId,
     eas: {

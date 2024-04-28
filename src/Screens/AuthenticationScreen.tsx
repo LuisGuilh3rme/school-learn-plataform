a21a27ba56e2abc714e598817113c7ff20e68d13
LuisGuih3rme <luislebert01@outlook.com>
2024-04-28 00:18:30 -0300

    feat: implements authentication screen structure

---
+++ src/Screens/AuthenticationScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function AuthenticationScreen() {
+  return (
+    <View>
+      <Text>Authentication Screen</Text>
+    </View>
+  );
+}

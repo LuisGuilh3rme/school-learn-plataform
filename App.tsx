fcecb999fafa8bf2e5fd5d2268ac377f7e9797c9
LuisGuih3rme <luislebert01@outlook.com>
2024-04-28 19:07:15 -0300

    refactor: change screen folder to lowercase

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 
-import AuthenticationScreen from "./src/Screens/AuthenticationScreen";
+import AuthenticationScreen from "./src/screens/authentication/AuthenticationScreen";
 
 const Stack = createNativeStackNavigator();
 

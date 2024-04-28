d7f58711a8835aea93d273e2b47349b0fd2c5f00
LuisGuih3rme <luislebert01@outlook.com>
2024-04-27 23:07:42 -0300

    chore: add typescript testing-library

--- App.test.tsx
+++ App.test.tsx
@@ -1,11 +1,15 @@
+import { render, screen } from "@testing-library/react-native";
 import React from "react";
-import renderer from "react-test-renderer";
 
 import App from "./App";
 
 describe("<App />", () => {
-  it("has 1 child", () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree.children.length).toBe(1);
+  it("have the text rendering", () => {
+    render(<App />);
+
+    const text = screen.getByText(
+      "Open up App.tsx to start working on your app!",
+    );
+    expect(text).toBeTruthy();
   });
 });

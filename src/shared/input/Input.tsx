8d878d1fc6749b1c7ce851325af37996732ad95a
LuisGuih3rme <luislebert01@outlook.com>
2024-04-29 22:16:54 -0300

    feat: create Input component

---
+++ src/shared/input/Input.tsx
@@ -0,0 +1,40 @@
+import { FieldError } from "react-hook-form";
+import {
+  View,
+  TextInput,
+  Text,
+  StyleSheet,
+  TextInputProps,
+} from "react-native";
+
+type InputProps = {
+  error?: FieldError;
+  errorText?: string;
+} & TextInputProps;
+
+export default function Input(props: InputProps) {
+  return (
+    <View>
+      <TextInput
+        style={[styles.input, props.error && { borderColor: "red" }]}
+        {...props}
+      />
+      {props.errorText && (
+        <Text style={styles.errorText}>{props.errorText}</Text>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 2,
+    borderRadius: 5,
+  },
+  errorText: {
+    color: "red",
+    marginTop: 1,
+  },
+});

fe5b6f6cda137641053cf17e79f2aa141580ad29
LuisGuilh3rme <luislebert01@outlook.com>
2024-05-26 14:57:57 -0300

    feat: implements draggable icon

--- src/shared/components/userIcon/UserIcon.component.tsx
+++ src/shared/components/userIcon/UserIcon.component.tsx
@@ -1,31 +1,59 @@
 import Feather from "@expo/vector-icons/Feather";
-import React from "react";
-import { StyleSheet, Pressable, View, Image } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Pressable,
+  View,
+  Image,
+  GestureResponderEvent,
+  PanResponderGestureState,
+} from "react-native";
+import Draggable from "react-native-draggable";
 
-import { useAppSelector } from "../../../../hooks";
+import { useAppDispatch, useAppSelector } from "../../../../hooks";
 import { NavigationProps } from "../../../types/Shared.types";
+import {
+  resetCoordinate,
+  setCoordinate,
+} from "../../reducers/Coordinates.reducer";
 
 export default function UserIcon(props: NavigationProps) {
   const userAvatar = useAppSelector((state) => state.avatar.value);
+  const coordinates = useAppSelector((state) => state.coordinates.value);
+  const dispatch = useAppDispatch();
+
+  const handleDrag = (
+    _e: GestureResponderEvent,
+    gestureState: PanResponderGestureState,
+  ) => {
+    try {
+      const { moveX, moveY } = gestureState;
+      dispatch(setCoordinate({ x: moveX, y: moveY }));
+    } catch (error) {
+      dispatch(resetCoordinate());
+    }
+  };
 
   return (
     <View style={styles.absoluteIcon}>
-      <Pressable
-        style={({ pressed }) => [
-          {
-            backgroundColor: pressed ? "rgb(220, 220, 220)" : "#f0f0f0",
-          },
-          styles.icon,
-        ]}
-        onPress={() => {
-          props.navigation.navigate("User");
-        }}
-      >
-        {userAvatar && (
-          <Image style={styles.avatar} source={{ uri: userAvatar }} />
-        )}
-        {!userAvatar && <Feather name="user" size={30} color="black" />}
-      </Pressable>
+      <Draggable x={coordinates.x} y={coordinates.y} onDrag={handleDrag}>
+        <Pressable
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed ? "rgb(220, 220, 220)" : "#f0f0f0",
+            },
+            styles.icon,
+          ]}
+          onPress={() => {
+            props.navigation.navigate("User");
+          }}
+        >
+          {userAvatar && (
+            <Image style={styles.avatar} source={{ uri: userAvatar }} />
+          )}
+          {!userAvatar && <Feather name="user" size={30} color="black" />}
+        </Pressable>
+      </Draggable>
     </View>
   );
 }
@@ -34,8 +62,6 @@ const styles = StyleSheet.create({
   absoluteIcon: {
     zIndex: 100,
     position: "absolute",
-    top: 50,
-    right: 20,
   },
   icon: {
     padding: 2,
--- store.ts
+++ store.ts
@@ -3,16 +3,19 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import persistReducer from "redux-persist/es/persistReducer";
 
 import AvatarReducer from "./src/shared/reducers/Avatar.reducer";
+import CoordinatesReducer from "./src/shared/reducers/Coordinates.reducer";
 import themeReducer from "./src/shared/reducers/Theme.reducer";
 
 const reducers = combineReducers({
   isDarkTheme: themeReducer,
   avatar: AvatarReducer,
+  coordinates: CoordinatesReducer,
 });
 
 const persistConfig = {
   key: "root",
   storage: AsyncStorage,
+  blacklist: ["coordinates"],
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
